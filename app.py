76784517c341bb968ed0e08641029db667fca6b4
wangxy1030 <wa0012yi@e.ntu.edu.sg>
2025-03-14 11:53:40 +0000

    1

--- app.py
+++ app.py
@@ -7,9 +7,16 @@ def index():
 def main():
     user_name=request.form.get("q")
     return(render_template("main.html"))
+@app.route("/foodexp",methods=["POST","GET"])
+def foodexp():
+    return(render_template("foodexp.html"))
 @app.route("/ethical_test",methods=["POST","GET"])
 def ethical_test():
     return(render_template("ethical_test.html"))
+@app.route("/foodexp_pred",methods=["POST","GET"])
+def foodexp_pred():
+    q=float(request.form.get("q"))
+    return(render_template("foodexp_pred.html",r=(q*0.4851)+147.4))
 @app.route("/test_result",methods=["POST","GET"])
 def test_result():
     answer=request.form.get("answer")
