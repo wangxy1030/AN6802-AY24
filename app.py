4c1b12dbfd6f427819a09d44b42ea720797fb069
wangxy1030 <wa0012yi@e.ntu.edu.sg>
2025-03-21 12:50:35 +0000

    1

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
 from flask import Flask,request,render_template
+import sqlite3
+import datetime
+import google.generativeai as genai
+import os
+api="AIzaSyCmzFIXPikSrximyftTJBJMkjkJ0ViqBpc"
+model=genai.GenerativeModel("gemini-1.5-flash")
+genai.configure(api_key=api)
 app=Flask(__name__)
+flag=1
 @app.route("/",methods=["POST","GET"])
 def index():
     return(render_template("index.html"))
 @app.route("/main",methods=["POST","GET"])
 def main():
-    user_name=request.form.get("q")
+    global flag
+    if flag==1:
+        t=datetime.datetime.now()
+        user_name=request.form.get("q")
+        conn=sqlite3.connect("user.db")
+        c=conn.cursor()
+        c.execute("insert into user (name, timestamp) values (?,?)", (user_name,t))
+        conn.commit()
+        c.close()
+        conn.close
+        flag=0
     return(render_template("main.html"))
 @app.route("/foodexp",methods=["POST","GET"])
 def foodexp():
@@ -17,6 +35,13 @@ def ethical_test():
 def foodexp_pred():
     q=float(request.form.get("q"))
     return(render_template("foodexp_pred.html",r=(q*0.4851)+147.4))
+@app.route("/FAQ",methods=["POST","GET"])
+def FAQ():
+    return(render_template("FAQ.html"))
+@app.route("/FAQ1",methods=["POST","GET"])
+def FAQ1():
+    r=model.generate_content("Factors for Profit")
+    return(render_template("FAQ1.html",r=r.candidates[0].content.parts[0]))
 @app.route("/test_result",methods=["POST","GET"])
 def test_result():
     answer=request.form.get("answer")
@@ -24,5 +49,26 @@ def test_result():
         return(render_template("pass.html"))
     elif answer=="true":
         return(render_template("fail.html"))
+@app.route("/userLog",methods=["POST","GET"])
+def userLog():
+    conn=sqlite3.connect("user.db")
+    c=conn.cursor()
+    c.execute("select * from user")
+    r=""
+    for row in c:
+        r=r+str(row)+"\n"
+    print(r)
+    c.close()
+    conn.close
+    return(render_template("userLog.html",r=r))
+@app.route("/deleteLog",methods=["POST","GET"])
+def deleteLog():
+    conn=sqlite3.connect("user.db")
+    c=conn.cursor()
+    c.execute("delete from user")
+    conn.commit()
+    c.close()
+    conn.close
+    return(render_template("deleteLog.html"))
 if __name__=="__main__":
     app.run()
